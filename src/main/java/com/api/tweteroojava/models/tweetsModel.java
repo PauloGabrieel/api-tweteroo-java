d75fdcce31fcab1a3d99ed88760fc58261428e47
PauloGabrieel <gabrieeel.c@gmail.com>
2023-03-30 17:44:27 -0300

    create tweets model

---
+++ src/main/java/com/api/tweteroojava/models/tweetsModel.java
@@ -0,0 +1,30 @@
+package com.api.tweteroojava.models;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tweets")
+public class tweetsModel {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String userName;
+
+    @Column(nullable = false)
+    private String avatar;
+
+    @Column(nullable = false)
+    private String tweet;
+}
