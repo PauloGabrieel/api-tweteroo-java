73fc2325de95864c027f23f2fb4fa98030c411db
PauloGabrieel <gabrieeel.c@gmail.com>
2023-03-30 15:33:03 -0300

    Start the project

---
+++ src/main/java/com/api/tweteroojava/TweterooJavaApplication.java
@@ -0,0 +1,13 @@
+package com.api.tweteroojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweterooJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweterooJavaApplication.class, args);
+	}
+
+}
